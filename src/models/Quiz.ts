64ef25b2a954fb4c6ac1998291b686d8aee1cd80
Jack <mrzzcn@gmail.com>
2020-08-09 22:20:45 +0800

    :memo: Prepare data

---
+++ src/models/Quiz.ts
@@ -0,0 +1,5 @@
+class Quiz {
+  
+}
+
+export default Quiz;
---
+++ src/models/book.ts
@@ -0,0 +1,12 @@
+const book = {
+  url: 'https://m.igetget.com/hybrid/v2/ebook/detail?bid=AnKgjNlArlXVNGKD1yL2Ek6YaogQv0Y8pvwbZ4RBn7j589zJpMxmdPeqOYDxqL4y',
+  douban: 'https://book.douban.com/subject/35119337/',
+  title: '数商',
+  subTitle: '如何用数据思考和行动，并在智能时代胜出',
+  author: '涂子沛',
+  publisher: '中信出版集团',
+  avatar: require('../assets/logo.jpg'),
+  ISBN: '9787521718621'
+};
+
+export default book;
